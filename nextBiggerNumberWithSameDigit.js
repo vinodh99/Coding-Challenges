355a3ab0688d1e3afdc9c3d081a980e7cc4b8182
vinodh tankasala <vinodh2019@gmail.com>
2019-01-24 20:54:44 -0500

    added next bigger number

---
+++ nextBiggerNumberWithSameDigit.js
@@ -0,0 +1,20 @@
+// You have to create a function that takes a positive integer number and returns the next bigger number formed by the same digits:
+
+function nextBigger(n){
+  let num =n.toString().split("")
+  let arr=num.map(Number).sort()
+  console.log(arr)
+  for(let i=num.length-1;i>=0;i--){
+      if(num[i]>num[i-1]){
+        let temp= num[i]
+        num[i]=num[i-1];
+        num[i-1]=temp;
+          if(num.join("")>n){
+            return Number(num.join(""));
+          }
+      }          
+      }
+  return -1;
+  }
+
+  nextBigger(91674)
