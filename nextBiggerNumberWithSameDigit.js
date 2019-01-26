a9f3383f7a9a92e9dfd33b77b20ea90dbf87267d
vinodh tankasala <vinodh2019@gmail.com>
2019-01-25 23:35:57 -0500

    added code

--- nextBiggerNumberWithSameDigit.js
+++ nextBiggerNumberWithSameDigit.js
@@ -2,21 +2,48 @@
 
 function nextBigger(n){
   let num =n.toString().split("")
-  let arr=num.map(Number).sort()
+  let arr=num.map(Number);
+  let array=arr
+  let array1=[];
   console.log(arr)
-  console.log(arr)
-  // added
-  for(let i=num.length-1;i>=0;i--){
-      if(num[i]>num[i-1]){
-        let temp= num[i]
-        num[i]=num[i-1];
-        num[i-1]=temp;
-          if(num.join("")>n){
-            return Number(num.join(""));
+  for(let j=arr.length-1;j>=0;j--){
+  for(let i=arr.length-1;i>=0;i--){
+      // if(arr[i]>arr[i-1]){
+        let temp= arr[i]
+        arr[i]=arr[i-1];
+        arr[i-1]=temp;
+            // console.log(Number(arr.join("")))
+
+          if(Number(arr.join(""))>n){
+            // console.log(arr.join(""))
+            array1.push(Number(arr.join("")));
           }
       }          
-      }
-  return -1;
+      // }
+      arr=array;
+    }
+    console.log(array1)
+  // return -1;
   }
 
-  nextBigger(91674)
+  nextBigger(8726510)
+
+  // expexted 8750126
+
+  // best solution
+
+  const sortedDigits = n => { let arr = n.toString().split(''); arr.sort((a, b) => b - a); return arr; };
+
+function nextBigger(n){
+  
+  let arr = sortedDigits(n);
+  let max = parseInt(arr.join(''), 10);
+  
+  for(var i = n + 1; i <= max; i++){
+    if(sortedDigits(i).every((x, j) => x === arr[j])){
+      return i;
+    }
+  }
+  
+  return -1;
+}
