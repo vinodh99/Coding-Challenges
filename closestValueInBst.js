c7d2ea2c8c74df9f21ad11d1636e95324e23d0bd
vinodh tankasala <vinodh2019@gmail.com>
2019-04-04 00:04:05 -0400

    updated closestValueBST

--- closestValueInBst.js
+++ closestValueInBst.js
@@ -1,29 +1,24 @@
 function findClosestValueInBst(tree, target) {
-    // Write your code here.
+  // Write your code here.
   // 	compare the difference
-      return findClosestValueInBstHelper(tree, target, tree.value);
+  return findClosestValueInBstHelper(tree, target, tree.value);
+}
+
+function findClosestValueInBstHelper(tree, target, closest) {
+  if (tree === null) {
+    return closest;
   }
-  
-  function findClosestValueInBstHelper(tree, target, closest){
-      if(!tree){
-          return closest
-      }
-      if(Math.abs(target-closest)>Math.abs(target-tree.value){
-          closest=tree.value
-      }
-      if(target<tree.value){
-          return findClosestValueInBstHelper(tree.left, target, closest)
-      }
-      else if(taeget>tree.value){
-          return findClosestValueInBstHelper(tree.right, target, closest)
-      }
-      else {
-          return closest
-      }
+  if (Math.abs(target - closest) > Math.abs(target - tree.value)) {
+    closest = tree.value;
   }
-  
-      
-  
-  // Do not edit the line below.
-  exports.findClosestValueInBst = findClosestValueInBst;
-  
+  if (target < tree.value) {
+    return findClosestValueInBstHelper(tree.left, target, closest);
+  } else if (target > tree.value) {
+    return findClosestValueInBstHelper(tree.right, target, closest);
+  } else {
+    return closest;
+  }
+}
+
+// Do not edit the line below.
+exports.findClosestValueInBst = findClosestValueInBst;
