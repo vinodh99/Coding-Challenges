ddb530a08ea2a54d37127a99f952be20299b96d2
vinodh <vinodh2019@gmail.com>
2019-04-30 23:19:34 -0400

    added recursive for powerset

--- powerset.js
+++ powerset.js
@@ -1,13 +1,31 @@
-function powerset(array) {
-  let subsets = [[]];
-  for (let ele of array) {
-    let length = subsets.length;
-    for (let i = 0; i < length; i++) {
-      let currentSubset = subsets[i];
-      subsets.push([...currentSubset, ele]);
-    }
-  }
-  return subsets;
-}
+// function powerset(array) {
+//   let subsets = [[]];
+//   for (let ele of array) {
+//     let length = subsets.length;
+//     for (let i = 0; i < length; i++) {
+//       let currentSubset = subsets[i];
+//       subsets.push([...currentSubset, ele]);
+//     }
+//   }
+//   return subsets;
+// }
 
-console.log(powerSet([1, 2, 3]));
+console.log(powerset([1, 2, 3]));
+
+// recursive solution
+
+function powerset(array, index = null) {
+  if (index === null) {
+    index = array.length - 1;
+  } else if (index < 0) {
+    return [[]];
+  }
+  let ele = array[index];
+  let subset = powerset(array, index - 1);
+  console.log(subset);
+  for (let i = 0, len = subset.length; i < len; i++) {
+    let currentSubset = subset[i];
+    subset.push([...currentSubset, ele]);
+  }
+  return subset;
+}
